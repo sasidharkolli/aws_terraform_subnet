32027b79cdc72097da9602f11126b560a95fb764
sasidharkolli <64691422+sasidharkolli@users.noreply.github.com>
2020-05-02 19:38:59 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -4,7 +4,7 @@
 resource "aws_subnet" "public-subnet-1" {
   vpc_id            = "vpc-3faf9a57"
   availability_zone = "ap-south-1a"
-  cidr_block        = "10.0.0.0/1"
+  cidr_block        = "172.31.0.0/1"
   map_public_ip_on_launch = "true"
 
   tags = {
@@ -19,7 +19,7 @@ output "out_public-subnet-1_id" {
 resource "aws_subnet" "public-subnet-2" {
   vpc_id            = "vpc-3faf9a57"
   availability_zone = "ap-south-1b"
-  cidr_block        = "10.0.0.0/2"
+  cidr_block        = "172.31.0.0/2"
   map_public_ip_on_launch = "true"
 
   tags = {
@@ -36,7 +36,7 @@ output "out_public-subnet-2_id" {
 resource "aws_subnet" "private-subnet-1" {
   vpc_id            = "vpc-3faf9a57"
   availability_zone = "ap-south-1a"
-  cidr_block        = "10.0.0.0/3"
+  cidr_block        = "172.31.0.0/3"
   map_public_ip_on_launch = "false"
 
   tags = {
@@ -51,7 +51,7 @@ output "out_private-subnet-1_id" {
 resource "aws_subnet" "private-subnet-2" {
   vpc_id            = "vpc-3faf9a57"
   availability_zone = "ap-south-1b"
-  cidr_block        = "10.0.0.0/4"
+  cidr_block        = "172.31.0.0/4"
   map_public_ip_on_launch = "false"
 
   tags = {
